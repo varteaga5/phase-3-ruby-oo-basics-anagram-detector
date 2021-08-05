bc52ad8a0b55e64f6b08b7082784d6eb8afd4bb9
vincent <vincent.arteaga@gmail.com>
2021-08-05 16:09:16 -0700

    commit tree f55364c29a94e09e7a261cdff85f0f19a7ef0b11

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    # init with word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+      # create return arr
+      arr_of_anagrams = []
+
+        # split words into arr of letters
+        arr_of_letters = arr.map{|el| el.split('')}
+
+        # iterate over arr of letters
+        arr_of_letters.each do |el| 
+            # checks if sorted current element, and @word are equal  
+          if el.sort == @word.split('').sort 
+            # if true add current el to return arr
+            arr_of_anagrams << el.join('')
+          end
+        end
+        # implicit return
+        arr_of_anagrams
+    end
+end
